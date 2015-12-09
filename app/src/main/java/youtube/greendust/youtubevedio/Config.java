a4325ddfae61df5a3a5027851bc6ab4c04dad3ac
chisty <chistyinfo@gmail.com>
2015-12-10 00:02:30 +0600

    Initial commit

---
+++ app/src/main/java/youtube/greendust/youtubevedio/Config.java
@@ -0,0 +1,10 @@
+package youtube.greendust.youtubevedio;
+
+public class Config {
+	// Google Console APIs developer key
+	// Replace this key with your's
+	public static final String DEVELOPER_KEY = "AIzaSyAasVTx-68D6dH6K5oBFySUtJ2uyEWfivU";
+	
+	// YouTube video id
+	public static final String YOUTUBE_VIDEO_CODE = "2_TnC3fB1iA";
+}
