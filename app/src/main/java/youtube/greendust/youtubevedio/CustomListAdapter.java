9237caf4271320ea37ba5c623ed95d13632b3538
chisty <chistyinfo@gmail.com>
2015-12-17 00:54:40 +0600

    Tab hasbeen added

--- app/src/main/java/youtube/greendust/youtubevedio/CustomListAdapter.java
+++ app/src/main/java/youtube/greendust/youtubevedio/CustomListAdapter.java
@@ -4,7 +4,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 public class CustomListAdapter extends ArrayAdapter<String> {
@@ -27,11 +26,11 @@ LayoutInflater inflater=context.getLayoutInflater();
 View rowView=inflater.inflate(R.layout.mylist, null,true);
 
 TextView txtTitle = (TextView) rowView.findViewById(R.id.item);
-ImageView imageView = (ImageView) rowView.findViewById(R.id.icon);
+//ImageView imageView = (ImageView) rowView.findViewById(R.id.icon);
 //TextView extratxt = (TextView) rowView.findViewById(R.id.textView1);
 
 	txtTitle.setText(itemname[position]);
-	imageView.setImageResource(imgid[position]);
+//	imageView.setImageResource(imgid[position]);
 //extratxt.setText(""+itemname[position]);
 	return rowView;
 
